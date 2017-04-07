1aee1565cfe5cf5ae88f715faa39a4700345a138
jeremiah.lumontod@gmail.com <jeremiah.lumontod@gmail.com>
2017-04-07 21:25:01 +0400

    added small instructions and java code

---
+++ README.md
@@ -0,0 +1,45 @@
+# tls1.2
+
+this scripts is my reference on how i created certificates for a self-signed 
+composed of root authority, subordinate or immediate authority, and a server and
+client certificates for a 2 way authentication. 
+
+this can also be use in a 1 way authentication.
+
+i used tomcat8.5.13 on OSX. 
+
+this is the setup for 1 and 2 way authentication in server.xml of conf/server.xml
+
+
+	<!-- working config 1 way authentication-->
+	<Connector port="8443" maxHttpHeaderSize="8192"
+		maxThreads="150"
+		enableLookups="false" disableUploadTimeout="true"
+		acceptCount="100" scheme="https" secure="true"
+		SSLEnabled="true"
+		SSLCertificateFile="/Users/user/Desktop/folders/projs/refs/ca4/root/ca/intermediate/certs/tomcatserver.cert.pem"
+		SSLPassword="123456"
+		SSLCertificateKeyFile="/Users/user/Desktop/folders/projs/refs/ca4/root/ca/intermediate/private/tomcatserver.key.pem" 
+		SSLProtocol="TLSv1.2"
+		SSLCertificateChainFile="/Users/user/Desktop/folders/projs/refs/ca4/root/ca/intermediate/certs/root.sub.ca.chain.cert.pem"
+	/>
+	<!-- end, working config 1 way authentication-->
+
+	<!-- working config 2 way authentication for java client, Safari, Chrome, Firefox browsers-->
+    <Connector
+          protocol = "org.apache.coyote.http11.Http11Nio2Protocol"
+          port = "8443"
+          SSLEnabled = "true"
+          scheme = "https"
+          secure = "true"
+          clientAuth = "true"
+          sslProtocol = "TLS"
+          sslEnabledProtocols = "TLSv1.2"
+          keystoreFile = "/Users/user/Desktop/folders/projs/refs/ca4/root/ca/tomcatserver.jks"
+          keystorePass = "123456"
+          truststoreFile="/Users/user/Desktop/folders/projs/refs/ca4/root/ca/tomcatservertruststore.jks"
+          truststorePass="123456"
+          sessionTimeout = "86400"
+          sessionCacheSize = "10000"
+    />
+	<!-- end, working config 2 way authentication for java client-->
---
+++ TwoWaySSLClient.java
@@ -0,0 +1,53 @@
+package com.jml;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+
+public class TwoWaySSLClient {
+	public static void main(String[] args) {
+		
+		System.setProperty("javax.net.ssl.keyStoreType", "JKS");
+		System.setProperty("javax.net.ssl.trustStoreType", "JKS");
+		System.setProperty("javax.net.debug", "all");
+        System.setProperty("javax.net.ssl.keyStore", "/Users/user/Desktop/folders/projs/refs/ca4/root/ca/tomcatclient.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "123456");
+        System.setProperty("javax.net.ssl.trustStore", "/Users/user/Desktop/folders/projs/refs/ca4/root/ca/tomcatclienttruststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "123456");
+        System.setProperty("deployment.security.SSLv2Hello","false"); 
+        System.setProperty("deployment.security.SSLv3","false"); 
+        System.setProperty("deployment.security.TLSv1","false");
+        System.setProperty("deployment.security.TLSv1.1","false");
+        System.setProperty("deployment.security.TLSv1.2","true");
+        
+		SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+		try {
+			URL url = new URL("https://localhost:8443/");
+			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+			conn.setHostnameVerifier(new HostnameVerifier() {
+				public boolean verify(String arg0, SSLSession arg1) {
+					return true;
+				}
+			});
+			conn.setSSLSocketFactory(sslsocketfactory);
+			InputStream inputstream = conn.getInputStream();
+			InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
+			BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
+			String string = null;
+			while ((string = bufferedreader.readLine()) != null) {
+				System.out.println("Received " + string);
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
